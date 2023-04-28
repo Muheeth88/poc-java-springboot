9075020ce19cc43f0dcd74ad9996da55f9faf49a
Abdul_Muheeth <abdul.muheeth@smartdocs.ai>
2023-04-29 03:24:05 +0530

    initial commit

---
+++ src/main/java/com/demo/pocjavaspringboot/PocJavaSpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.demo.pocjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class PocJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/pocjavaspringboot/PocJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pocjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
