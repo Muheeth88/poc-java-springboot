8326171369fdf27cf35ecad15177318c6e43b4f4
Abdul_Muheeth <abdul.muheeth@smartdocs.ai>
2023-04-30 02:46:01 +0530

    service repository added

---
+++ src/test/java/com/demo/pocjavaspringboot/users/UsersServiceTest.java
@@ -0,0 +1,28 @@
+package com.demo.pocjavaspringboot.users;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.demo.pocjavaspringboot.users.dtos.CreateUserDto;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UsersServiceTest {
+    @Autowired 
+    UsersService usersService;
+    
+    // @MockBean
+    // UsersRepository usersRepository;
+
+    @Test
+    void canCreateUsers() {
+        var user = usersService.createUser(new CreateUserDto("Bruce Wayne", "bruce@batman.com", "caped"));
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("Bruce Wayne", user.getUserName());
+    }
+}
